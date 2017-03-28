da0b906d774bd3d5a4cb579f5346ed63a7d47529
RolandoT <rolando.tremont2@upr.edu>
2017-03-28 00:15:27 -0400

    Mine count changed to 10

--- Lab5/src/MyPanel.java
+++ Lab5/src/MyPanel.java
@@ -13,7 +13,7 @@ public class MyPanel extends JPanel {
 	private static final int INNER_CELL_SIZE = 29;
 	private static final int TOTAL_COLUMNS = 9;
 	private static final int TOTAL_ROWS = 9;
-	private static final int mine_COUNT = 13;
+	private static final int mine_COUNT = 10;
 	private Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
 	private boolean[][] mineArray = new boolean[TOTAL_COLUMNS][TOTAL_ROWS];
 	private int flagCounter;	//Keep track of the number of flags
