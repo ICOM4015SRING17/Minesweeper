01045a365ea4aa5e0aaa361207a6b3ae4ee30a3d
RolandoT <rolando.tremont2@upr.edu>
2017-03-28 00:11:32 -0400

    Code changed to use methods. Game Works.

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -3,35 +3,13 @@ import java.awt.Component;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Random;
-
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private Random generator = new Random();
-	private int m1x = generator.nextInt(9) + 1;
-	private int m1y = generator.nextInt(9) + 1;
-	private int m2x = generator.nextInt(9) + 1;
-	private int m2y = generator.nextInt(9) + 1;
-	private int m3x = generator.nextInt(9) + 1;
-	private int m3y = generator.nextInt(9) + 1;
-	private int m4x = generator.nextInt(9) + 1;
-	private int m4y = generator.nextInt(9) + 1;
-	private int m5x = generator.nextInt(9) + 1;
-	private int m5y = generator.nextInt(9) + 1;
-	private int m6x = generator.nextInt(9) + 1;
-	private int m6y = generator.nextInt(9) + 1;
-	private int m7x = generator.nextInt(9) + 1;
-	private int m7y = generator.nextInt(9) + 1;
-	private int m8x = generator.nextInt(9) + 1;
-	private int m8y = generator.nextInt(9) + 1;
-	private int m9x = generator.nextInt(9) + 1;
-	private int m9y = generator.nextInt(9) + 1;
-	private int m10x = generator.nextInt(9) + 1;
-	private int m10y = generator.nextInt(9) + 1;
-	public void mousePressed(MouseEvent e) {
+
+	public void mousePressed(MouseEvent e) {		//When mouse button is pressed
 		switch (e.getButton()) {
-		case 1:        //Left mouse button
+		case 1:		//Left mouse button
 			Component c = e.getComponent();
 			while (!(c instanceof JFrame)) {
 				c = c.getParent();
@@ -53,36 +31,36 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
 			myPanel.repaint();
 			break;
-		case 3:        //Right mouse button
-			Component r = e.getComponent();
-			while (!(r instanceof JFrame)) {
-				r = r.getParent();
-				if (r == null) {
+		case 3:		//Right mouse button
+			c = e.getComponent();
+			while (!(c instanceof JFrame)) {
+				c = c.getParent();
+				if (c == null) {
 					return;
 				}
 			}
-			JFrame myFrameR = (JFrame) r;
-			MyPanel myPanelR = (MyPanel) myFrameR.getContentPane().getComponent(0);
-			Insets myInsetsR = myFrameR.getInsets();
-			int x1R = myInsetsR.left;
-			int y1R = myInsetsR.top;
-			e.translatePoint(-x1R, -y1R);
-			int xR = e.getX();
-			int yR = e.getY();
-			myPanelR.x = xR;
-			myPanelR.y = yR;
-			myPanelR.mouseDownGridX = myPanelR.getGridX(xR, yR);
-			myPanelR.mouseDownGridY = myPanelR.getGridY(xR, yR);
-			myPanelR.repaint();	
+			myFrame = (JFrame) c;
+			myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
+			myInsets = myFrame.getInsets();
+			x1 = myInsets.left;
+			y1 = myInsets.top;
+			e.translatePoint(-x1, -y1);
+			x = e.getX();
+			y = e.getY();
+			myPanel.x = x;
+			myPanel.y = y;
+			myPanel.mouseDownGridX = myPanel.getGridX(x, y);
+			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
+			myPanel.repaint();
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
 	}
-	public void mouseReleased(MouseEvent e) {
+	public void mouseReleased(MouseEvent e) {		//When mouse button is released
 		switch (e.getButton()) {
-		case 1:        //Left mouse button
+		case 1:		//Left mouse button
 			Component c = e.getComponent();
 			while (!(c instanceof JFrame)) {
 				c = c.getParent();
@@ -91,7 +69,7 @@ public class MyMouseAdapter extends MouseAdapter {
 				}
 			}
 			JFrame myFrame = (JFrame)c;
-			MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+			MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
 			Insets myInsets = myFrame.getInsets();
 			int x1 = myInsets.left;
 			int y1 = myInsets.top;
@@ -100,88 +78,90 @@ public class MyMouseAdapter extends MouseAdapter {
 			int y = e.getY();
 			myPanel.x = x;
 			myPanel.y = y;
-
 			int gridX = myPanel.getGridX(x, y);
 			int gridY = myPanel.getGridY(x, y);
-			boolean mine1 = gridX == m1x && gridY == m1y;
-			boolean mine2 = gridX == m2x && gridY == m2y;
-			boolean mine3 = gridX == m3x && gridY == m3y;
-			boolean mine4 = gridX == m4x && gridY == m4y;
-			boolean mine5 = gridX == m5x && gridY == m5y;
-			boolean mine6 = gridX == m6x && gridY == m6y;
-			boolean mine7 = gridX == m7x && gridY == m7y;
-			boolean mine8 = gridX == m8x && gridY == m8y;
-			boolean mine9 = gridX == m9x && gridY == m9y;
-			boolean mine10 = gridX == m10x && gridY == m10y;
-			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
+			if (myPanel.mouseDownGridX == -1 || myPanel.mouseDownGridY == -1) {
+				//Had pressed outside
+				//Do nothing
 			} else {
 				if ((gridX == -1) || (gridY == -1)) {
+					//Is releasing outside
+					//Do nothing
 				} else {
-					if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
-					} else {
-						if ((gridX == 0) || (gridY == 0)) {
-
-						}else {
-							if (mine1 || mine2 || mine3 || mine4 || mine5 || mine6 || mine7 || mine8 || mine9 || mine10){
-								myPanel.colorArray[m1x][m1y] = Color.BLACK;
-								myPanel.colorArray[m2x][m2y] = Color.BLACK;
-								myPanel.colorArray[m3x][m3y] = Color.BLACK;
-								myPanel.colorArray[m4x][m4y] = Color.BLACK;
-								myPanel.colorArray[m5x][m5y] = Color.BLACK;
-								myPanel.colorArray[m6x][m6y] = Color.BLACK;
-								myPanel.colorArray[m7x][m7y] = Color.BLACK;
-								myPanel.colorArray[m8x][m8y] = Color.BLACK;
-								myPanel.colorArray[m9x][m9y] = Color.BLACK;
-								myPanel.colorArray[m10x][m10y] = Color.BLACK;
-
-
-								myPanel.repaint();
-								System.out.println("GAME OVER!!");
-								//							}else{
-								//								if(myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY].equals(Color.RED)){
-
-							} else {
-
-								myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.GRAY;
-								myPanel.repaint();
-							}
-
+					if (myPanel.mouseDownGridX != gridX || myPanel.mouseDownGridY != gridY) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					}
+					else {
+						//On the grid
+						if(myPanel.isMine(gridX, gridY) && myPanel.getColor(gridX,gridY) != Color.RED) {
+							//If square is a mine and hasn't been flagged, user lost
+							myPanel.minePressed();
+						} else if(myPanel.getColor(gridX,gridY) != Color.RED && myPanel.getColor(gridX,gridY) != Color.BLACK) {
+							//Paint the panel if it hasn't been flagged or revealed to be a mine
+							myPanel.setColor(gridX,gridY,Color.GRAY);
+							//Call the chain method to clear adjacent tiles
+							myPanel.chain(gridX, gridY);
+							//Check if user has met winning conditions
+							myPanel.winConditions();
 						}
+						myPanel.repaint();
 					}
 				}
 			}
-			//			}
 			myPanel.repaint();
-			break;
-		case 3:        //Right mouse button
-			Component r = e.getComponent();
-			while (!(r instanceof JFrame)) {
-				r = r.getParent();
-				if (r == null) {
+		case 3:		//Right mouse button
+			c = e.getComponent();
+			while (!(c instanceof JFrame)) {
+				c = c.getParent();
+				if (c == null) {
 					return;
 				}
 			}
-			JFrame myFrameR = (JFrame) r;
-			MyPanel myPanelR = (MyPanel) myFrameR.getContentPane().getComponent(0);
-			Insets myInsetsR = myFrameR.getInsets();
-			int x1R = myInsetsR.left;
-			int y1R = myInsetsR.top;
-			e.translatePoint(-x1R, -y1R);
-			int xR = e.getX();
-			int yR = e.getY();
-			myPanelR.x = xR;
-			myPanelR.y = yR;
-			myPanelR.mouseDownGridX = myPanelR.getGridX(xR, yR);
-			myPanelR.mouseDownGridY = myPanelR.getGridY(xR, yR);
-			myPanelR.repaint();	
-
-			myPanelR.colorArray[myPanelR.mouseDownGridX][myPanelR.mouseDownGridY] = Color.RED;
-			myPanelR.repaint();
+			myFrame = (JFrame)c;
+			myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
+			myInsets = myFrame.getInsets();
+			x1 = myInsets.left;
+			y1 = myInsets.top;
+			e.translatePoint(-x1, -y1);
+			x = e.getX();
+			y = e.getY();
+			myPanel.x = x;
+			myPanel.y = y;
+			gridX = myPanel.getGridX(x, y);
+			gridY = myPanel.getGridY(x, y);
+			if (myPanel.mouseDownGridX == -1 || myPanel.mouseDownGridY == -1) {
+				//Had pressed outside
+				//Do nothing
+			} else {
+				if ((gridX == -1) || (gridY == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					} else {
+						//On the grid
+						if(myPanel.getColor(gridX,gridY) == Color.RED) {
+							//If tile is flagged, paint it white
+							myPanel.setColor(gridX,gridY,Color.WHITE);
+							//Lower the counter by 1
+							myPanel.removeFlag();
+						} else if(myPanel.getColor(gridX,gridY) != Color.GRAY && myPanel.getColor(gridX,gridY) != Color.BLACK && myPanel.getFlags() < myPanel.getMines()){
+							//Paint the tile red as long as it hasn't been uncovered or shown to be a mine
+							//Increase counter by 1
+							myPanel.addFlag();
+							myPanel.setColor(gridX,gridY,Color.RED);
+						} 
+					}
+				}
+			}
+			myPanel.repaint();
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
-
 	}
 }
--- Lab5/src/MyPanel.java
+++ Lab5/src/MyPanel.java
@@ -1,8 +1,9 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.util.Random;
-
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
@@ -10,14 +11,18 @@ public class MyPanel extends JPanel {
 	private static final int GRID_X = 25;
 	private static final int GRID_Y = 25;
 	private static final int INNER_CELL_SIZE = 29;
-	private static final int TOTAL_COLUMNS = 10;
-	private static final int TOTAL_ROWS = 11;   //Last row has only one cell
+	private static final int TOTAL_COLUMNS = 9;
+	private static final int TOTAL_ROWS = 9;
+	private static final int mine_COUNT = 13;
+	private Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	private boolean[][] mineArray = new boolean[TOTAL_COLUMNS][TOTAL_ROWS];
+	private int flagCounter;	//Keep track of the number of flags
 	public int x = -1;
 	public int y = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
-	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
-	public MyPanel() {   //This is the constructor... this code runs first to initialize
+
+	public MyPanel() {
 		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
 		}
@@ -27,19 +32,11 @@ public class MyPanel extends JPanel {
 		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
-			colorArray[x][0] = Color.LIGHT_GRAY;
-		}
-		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
-			colorArray[0][y] = Color.LIGHT_GRAY;
-		}
-		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
-			for (int y = 1; y < TOTAL_ROWS; y++) {
-				colorArray[x][y] = Color.WHITE;
-			}
-		}
+		
+		this.setUp(); //Runs tile and mine setup method
 	}
-	public void paintComponent(Graphics g) {
+	
+	public void paintComponent(Graphics g) {	//Method in charge of painting tiles and numbers
 		super.paintComponent(g);
 
 		//Compute interior coordinates
@@ -55,31 +52,56 @@ public class MyPanel extends JPanel {
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect(x1, y1, width + 1, height + 1);
 
-		//Draw the grid minus the bottom row (which has only one cell)
-		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
+		//Draw the grid
 		g.setColor(Color.BLACK);
-		for (int y = 0; y <= TOTAL_ROWS - 1; y++) {
+		for (int y = 0; y <= TOTAL_ROWS; y++) {
 			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
 		}
 		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
-			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS )));
 		}
 
-		//Draw an additional cell at the bottom left
-		g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
-
-		//Paint cell colors
+		//Paint cell colors and paint necessary numbers
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
 			for (int y = 0; y < TOTAL_ROWS; y++) {
-				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
-					Color c = colorArray[x][y];
-					g.setColor(c);
-					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+				Color c = this.getColor(x, y);
+				g.setColor(c);
+				g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+				
+				//If the tile has been pressed, count surrounding mines
+				if (this.getColor(x, y) == Color.GRAY) {
+					int mineCount = this.surroundingMines(x, y);
+					//Call the method to paint the number
+					this.paintNumber(g, mineCount, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 10, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 22);
 				}
+			}
+		}	
+	}
+	public void setUp() {	//This method paints the tiles and sets the mine locations, used to reset board
+		flagCounter = 0; //Set the flag count to 0
+		
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Delete any existing mines
+			for (int y = 0; y < TOTAL_ROWS; y++) {
+				mineArray[x][y] = false;
+			}
+		}
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Paint all tiles to their covered color
+			for (int y = 0; y < TOTAL_ROWS; y++) {
+				this.setColor(x, y, Color.WHITE);
+			}
+		}
+		for (int x = 0; x < mine_COUNT;) {		//Assign mines to random spaces
+			int i = new Random().nextInt(TOTAL_COLUMNS);
+			int j = new Random().nextInt(TOTAL_ROWS);
+			if(mineArray[i][j] == false) { 		//Assigns a mine if the space doesn't have one already
+				mineArray[i][j] = true;
+				x++;
 			}
 		}
 	}
-	public int getGridX(int x, int y) {
+	
+	//Setter and getter methods
+	public int getGridX(int x, int y) {		//Return column
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
 		int y1 = myInsets.top;
@@ -96,15 +118,12 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
-			return x;
-		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 1) {   //Outside the rest of the grid
 			return -1;
 		}
 		return x;
 	}
-	public int getGridY(int x, int y) {
+	public int getGridY(int x, int y) {		//Return row
 		Insets myInsets = getInsets();
 		int x1 = myInsets.left;
 		int y1 = myInsets.top;
@@ -121,12 +140,142 @@ public class MyPanel extends JPanel {
 		}
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
-		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
-			return y;
-		}
-		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS -1) {   //Outside the rest of the grid
 			return -1;
 		}
 		return y;
+	}
+	public int getColumns() {	//Returns number of columns
+		return TOTAL_COLUMNS;
+	}
+	public int getRows() {		//Returns number of rows
+		return TOTAL_ROWS;
+	}
+	public Color getColor(int x, int y) {		//Returns the color of the tile
+		return colorArray[x][y];
+	}
+	public void setColor(int x, int y, Color color) {		//Sets the color of a specific tile
+		colorArray[x][y] = color;
+		return;
+	}
+	public int getMines() {		//Returns the number of mines
+		return mine_COUNT;
+	}
+	public boolean isMine(int x, int y) {	//Method to verify if the selected square is a mine
+		return mineArray[x][y];
+	}
+	public void addFlag() {		//Method to add 1 to flag counter
+		flagCounter++;
+		
+	}
+	public void removeFlag() {		//Method to subtract 1 to flag counter
+		flagCounter--;
+	}
+	public int getFlags() {		//Returns the number of flags
+		return flagCounter;
+	}
+	
+	public void paintNumber(Graphics g, int mineCount, int x, int y) {		//Method to paint each number with a different color and display it
+		switch(mineCount){
+		case 0:
+			break;
+		case 1:
+			g.setColor(Color.BLUE);
+			break;
+		case 2:
+			g.setColor(Color.GREEN);
+			break;
+		case 3:
+			g.setColor(Color.RED);
+			break;
+		case 4:
+			g.setColor(Color.YELLOW);
+			break;
+		case 5:
+			g.setColor(Color.ORANGE);
+			break;
+		case 6:
+			g.setColor(Color.MAGENTA);
+			break;
+		case 7:
+			g.setColor(Color.PINK);
+			break;
+		case 8:
+			g.setColor(Color.WHITE);
+			break;
+			
+		}
+		g.setFont(new Font("default", Font.BOLD, 18));
+		g.drawString(mineCount + "" + "", x, y);
+	}
+	public int surroundingMines(int x, int y) {		//Method for counting number of mines around a tile.
+		
+		int mineCounter = 0;
+		
+		for(int i = x-1; i <= x+1; i++) {
+			for(int j = y-1; j <= y+1; j++) {
+				//Check if coordinates are inside the grid
+				if(i < this.getColumns() && i >= 0 && j < this.getRows() && j >= 0 ) {
+					if(isMine(i,j)) {
+						mineCounter++;
+					}
+				}
+			}
+		}
+		return mineCounter;
+	}
+	public void chain(int x, int y) { 	//Recursive method to uncover a chain of tiles that have no surrounding mines
+		//Check if there aren't any surrounding mines
+		if(this.surroundingMines(x, y) == 0) {
+			for(int i=x-1; i<=x+1; i++) {
+				for (int j=y-1; j<=y+1; j++) {
+					//Check if coordinates are inside the grid
+					if(i < this.getColumns() && i >= 0 && j < this.getRows() && j >= 0) {
+						//Check that tile is uncovered
+						if(this.getColor(i,j) == Color.WHITE){
+							this.setColor(i, j, Color.GRAY);
+							this.chain(i, j);
+						}
+					}
+				}
+			}
+		}
+	}
+	public void minePressed() {		//Method for when user loses the game. Uncovers all tiles
+		for(int i = 0; i < this.getColumns(); i++) {
+			for(int j = 0; j < this.getRows(); j++) {
+				if(this.isMine(i,j)) {
+					this.setColor(i,j,Color.BLACK);
+				} else {
+					this.setColor(i, j, Color.GRAY);
+				}
+			}
+		}
+		
+		repaint();
+		int n = JOptionPane.showConfirmDialog(null,"Try Again?","GAME OVER!!",JOptionPane.YES_NO_OPTION); //Show message prompt if game is over
+        if(n == JOptionPane.YES_OPTION){
+        	this.setUp();
+        } else {
+         	System.exit(0);
+        }
+	}
+	public void winConditions(){	//Check if the player has won
+		for(int i = 0; i < this.getColumns(); i++) {
+			for(int j = 0; j < this.getRows(); j++) {
+				if((this.getColor(i,j) == Color.WHITE || this.getColor(i,j) == Color.RED) && this.isMine(i,j) == false){
+					//If there are still covered tiles that are not mines, game hasn't ended.
+					return;
+				}
+			}
+		}
+
+		repaint();
+		int n = JOptionPane.showConfirmDialog(null,"Try Again?","You Win!! :)",JOptionPane.YES_NO_OPTION); //Show message box after game has ended
+        if(n == JOptionPane.YES_OPTION){ //Prompt user to try again
+        	this.setUp();
+        } else {
+         	System.exit(0);
+        }
 	}
 }
